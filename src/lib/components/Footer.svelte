e65eb52922dc51055471032e8143b44eea57cb3a
Tommy Wong <tommywong.dev@gmail.com>
2022-08-09 11:57:19 +0800

    Add footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,29 @@
+<script>
+	import FacebookIcon from './FacebookIcon.svelte';
+	import TwitterIcon from './TwitterIcon.svelte';
+	import YoutubeIcon from './YoutubeIcon.svelte';
+</script>
+
+<footer class="p-10 footer bg-primary text-primary-content footer-center">
+	<div class="grid grid-flow-col gap-4">
+		<a href="/projects" class="link link-hover">Projects</a>
+		<a href="/posts" class="link link-hover">Bout</a>
+		<a href="/about" class="link link-hover">About</a>
+	</div>
+	<div>
+		<div class="grid grid-flow-col gap-4">
+			<a href="https://twitter.com/tommywong_dev" target="_blank">
+				<TwitterIcon />
+			</a>
+			<a href="https://www.youtube.com/channel/UCkO6WyurJjBc7KP9jbrIb0Q" target="_blank">
+				<YoutubeIcon />
+			</a>
+			<a href="https://www.facebook.com/profile.php?id=100004680791150" target="_blank">
+				<FacebookIcon />
+			</a>
+		</div>
+	</div>
+	<div>
+		<p>Copyright &copy; {`${new Date().getFullYear()}`} - All right reserved by ME</p>
+	</div>
+</footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,9 @@
 <script>
+	import Footer from '$lib/components/Footer.svelte';
 	import '../app.css';
 </script>
 
 <div class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </div>
+<Footer />
